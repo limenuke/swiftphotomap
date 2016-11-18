8d2a076bd1dd082523683adb7d343fb46bb093d0
limenuke <consid3r@gmail.com>
2016-11-17 19:10:32 -0800

    On Milestone 4

--- Photo Map/PhotoMapViewController.swift
+++ Photo Map/PhotoMapViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import MapKit
 
-class PhotoMapViewController: UIViewController {
+class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, LocationsViewControllerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var camButton: UIButton!
+    var savedImage : UIImage?
+    var imageDict : [String : AnyObject?]?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        //one degree of latitude is approximately 111 kilometers (69 miles) at all times.
+        let sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667),
+                                              MKCoordinateSpanMake(0.1, 0.1))
+        mapView.setRegion(sfRegion, animated: false)
+        camButton.layer.cornerRadius = 0.7 * camButton.bounds.size.width
+        camButton.clipsToBounds = true
+        camButton.backgroundColor = UIColor.white
+        
         // Do any additional setup after loading the view.
     }
 
@@ -21,15 +35,51 @@ class PhotoMapViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func onCamera(_ sender: AnyObject) {
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        if (UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera)) {
+        vc.sourceType = UIImagePickerControllerSourceType.camera
+        } else {
+            vc.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        }
+        self.navigationController?.present(vc, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        // Get the image captured by the UIImagePickerController
+        print ("delegate achieved!\n\n")
+        //let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        savedImage = editedImage
+        imageDict = info
+        print ("delegate achieved\n\n")
+        // Do something with the images (based on your use case)
+        
+        // Dismiss UIImagePickerController to go back to your original view controller
+        self.navigationController?.dismiss(animated: true, completion: {
+                self.performSegue(withIdentifier: "tagSegue", sender: nil)
+            }
+        )
+    }
+    
+    func locationsPickedLocation(controller: LocationsViewController, latitude: NSNumber, longitude: NSNumber) {
+        
+    }
 
-    /*
+
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    override func prepare(for segue: UIStoryboardSegue, sender: AnyObject?) {
+        if (segue.identifier == "tagSegue") {
+            let vc = segue.destination as! LocationsViewController
+            vc.delegate = self
+        }
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
 
 }
